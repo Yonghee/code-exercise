2d249e52ee4cd987ba97d2275ba0903115260f48
Yonghee <yhlee73@gmail.com>
2015-01-13 22:02:12 +0900

    Initial commit

---
+++ src/com/yonghee/exercise/behaviorparam/FruitFilter.java
@@ -0,0 +1,24 @@
+package com.yonghee.exercise.behaviorparam;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Created by yhlee on 15. 1. 13..
+ */
+public class FruitFilter {
+
+    public static <T> List<T> filter(List<T> fruits, Predicate<T> predicate) {
+
+        List<T> result = new ArrayList<T>();
+        for (T item : fruits) {
+            if (predicate.test(item)) {
+               result.add(item);
+            }
+        }
+
+        return result;
+    }
+
+}
---
+++ src/com/yonghee/exercise/behaviorparam/FruitFilterTest.java
@@ -0,0 +1,51 @@
+package com.yonghee.exercise.behaviorparam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class FruitFilterTest {
+
+    @org.junit.Test
+    public void testFilter() throws Exception {
+
+        List<Apple> apples = new ArrayList<Apple>();
+        apples.add(new Apple("red", 10));
+        apples.add(new Apple("blue", 22));
+        apples.add(new Apple("green", 5));
+
+        List<Apple> result = FruitFilter.filter(apples, (Apple apple)->"red".equals(apple.getColor()));
+
+        assertEquals(1,result.size());
+
+
+    }
+}
+
+class Apple {
+
+    private String color;
+    private int weight;
+
+    public Apple(String color, int weight) {
+        this.color = color;
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
